b19ec2749d4a4b7e15794437fc03fc54bfe7f11d
Sunjay Dhama <sdsunjay73@yahoo.com>
2017-08-02 00:46:47 -0700

    Update how we register for an account

--- js/register.js
+++ js/register.js
@@ -1,82 +1,67 @@
 $(document).on('click', '#submit', function() { // catch the form's submit event
-   //if(regformhash(document.getElementById("username").value,document.getElementById("email").value,document.getElementById("password").value,document.getElementById("password1").value,document.getElementById("answer1").value,document.getElementById("answer2").value))
-   if( $('#username').val().length > 2){
-         if($('#username').val().length < 64) {
-             if(validateEmail($('#email').val())){
-               if ($('#password').val().length > 9 ){
-                  if ($('#password').val().length < 72){
-                     if ($('#password1').val().length > 9){
-                        if ($('#password1').val().localeCompare($('#password').val()) == 0)
-                           {
-                              if ($('#answer1').val().length > 7){
-                                 if ($('#answer1').val().length < 128){
-                                    if (validateQuestion($('#question1').val())){
-                                       $.ajax({
-                                          method: "POST",
-                                          url: "register.php",
-                                          data: {
-                                             action : "register",
-                                             username: $("#username").val(),
-                                             email: $("#email").val(),
-                                             password: $("#password").val(),
-                                             password1: $("#password1").val(),
-                                             question1: $("#question1").val(),
-                                             answer1: $("#answer1").val()
-                                       } //send registration information to register.php
-                                      // type: 'post',
-                                      // async: true,
-                                    })
-                                      .done(function( result ) {
-                                             if (result.toLowerCase() == "yes")
-                                             {
-                                                //alert("toLowerCase");
-                                                window.location = "protected_page.php";
-                                             }
-                                             if (result.localeCompare("yes") == 0){
-                                                //alert("localeCompare");
-                                                window.location = "protected_page.php";
-                                              }
-                                             else{
-                                                //bug here needs to be fixed
-                                              //  alert(result);
-                                                window.location = "protected_page.php";
-                                                //window.location = "register.html";
-                                             }
-                                      })
-                                      .fail(function() {
-                                          alert('A network error has occurred. Please try again.');
-                                      });
-                                      }  else {
-                                       alert('Error with Question 1.');
-                                    }           
-                                 }  else {
-                                    alert('Answer1 must be less than 128 characters.');
-                                 }           
-                              } else {
-                                 alert('Answer1 must be greater than 7 characters.');
-                              }           
-                           } else {
-                              alert('Passwords do not match.');
+   if(regformhash(document.getElementById("username").value,document.getElementById("email").value,document.getElementById("password").value,document.getElementById("password1").value,document.getElementById("answer1").value)){
+      if(validateEmail($('#email').val())){
+         if ($('#password').val().length > 7 ){
+            if ($('#password').val().length < 72){
+               if ($('#password1').val().length > 7){
+                  if ($('#password1').val().localeCompare($('#password').val()) == 0){
+                     if ($('#answer1').val().length > 7){
+                        if ($('#answer1').val().length < 128){
+                           if (validateQuestion($('#question1').val())){
+                              $.ajax({
+                                 method: "POST",
+                                 url: "register.php",
+                                 type: 'POST',
+                                 data: {
+                                    action : "register",
+                                    name: $("#name").val(),
+                                    username: $("#username").val(),
+                                    email: $("#email").val(),
+                                    password: $("#password").val(),
+                                    password1: $("#password1").val(),
+                                    question1: $("#question1").val(),
+                                    answer1: $("#answer1").val()
+                                 } //send registration information to register.php
+                              })
+                              .done(function(responseData) {
+                                 var parsed_data = JSON.parse(responseData);
+                                 //console.log(responseData); // works. outputs to console success
+                                 if (parsed_data.response.localeCompare("yes") == 0){
+                                    window.location = "protected_page.php";
+                                 }
+                                 else{
+                                    alert(parsed_data.response);
+                                 }
+                              })
+                              .fail(function(responseData) {
+                                 alert('A network error has occurred. Please try again.');
+                              });
+                           }  else {
+                              alert('Error with Question 1.');
                            }           
-                     }
-                     else {
-                        alert('Password is of insufficient length.');
+                        }  else {
+                           alert('Answer1 must be less than 128 characters.');
+                        }           
+                     } else {
+                        alert('Answer1 must be greater than 7 characters.');
                      }           
                   } else {
-                     alert('Password must be less than 72 characters.');
+                     alert('Passwords do not match.');
                   }           
-               } else {
+               }
+               else {
                   alert('Password is of insufficient length.');
                }           
-             } else {
-                alert('Please fix email field.');
-             }           
+            } else {
+               alert('Password must be less than 72 characters.');
+            }           
          } else {
-            alert('Username must be between 3 and 64 characters.');
+            alert('Password is of insufficient length.');
          }           
-   } else {
-      alert('Username must be between 3 and 64 characters.');
-   }           
+      } else {
+         alert('Please fix email field.');
+      }
+   }
    return false; // cancel original event to prevent form submitting
 });
 
@@ -94,4 +79,7 @@ function validateQuestion(n){
       return true;
    else
       return false;
+}
+function hasWhiteSpace(s) {
+   return /\s/g.test(s);
 }
--- register.php
+++ register.php
@@ -7,8 +7,9 @@ include_once("functions.php");
 
 sec_session_start(); // Our custom secure way of starting a PHP session.
 
-if (isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['password1'],$_POST['question1'],$_POST['answer1'])) {
+if (isset($_POST['name'], $_POST['username'], $_POST['email'], $_POST['password'], $_POST['password1'],$_POST['question1'],$_POST['answer1'])) {
    // Sanitize and validate the data passed in
+   $name = $_POST['name'];
    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
    $password = $_POST['password'];
@@ -16,30 +17,24 @@ if (isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['passw
    $answer1 = filter_input(INPUT_POST, 'answer1', FILTER_SANITIZE_STRING);
    $question1 = $_POST['question1'];
 
-   if(registerUser($username,$email,$password,$password1))
-   {
-      if(insertQuestionsAnswers($question1,$answer1,$username))
-      {
+   if(registerUser($name, $username,$email,$password,$password1)) {
+      if(insertQuestionsAnswers($question1,$answer1,$username)) {
          // Login success 
          $_SESSION['user_name'] = $username;
          $_SESSION['user_is_logged_in'] = true;         
-         // $_SESSION['user']=1;
-         $session_name = 'sec_session_id';   // Set a custom session name
-
-         // Sets the session name to the one set above.
-         session_name($session_name);
-         echo "yes";
+         sec_session_start();
+         
+         $arr = array ('response'=>'yes');
+         echo json_encode($arr);
       }
-      else
-      {
-         //echo $feedback;
-         echo $_SESSION['Error']; 
+      else{
+         $arr = array ('response'=>$_SESSION['Error']);
+         echo json_encode($arr);
       }
    }
-   else
-   {
-      echo $_SESSION['Error']; 
-      //echo $feedback;
+   else {
+      $arr = array ('response'=>$_SESSION['Error']);
+      echo json_encode($arr);
    }
 }
 
