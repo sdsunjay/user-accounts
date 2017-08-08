f9a4112d3bdec9180da155fd424225c9b9e57fb3
Sunjay Dhama <sdsunjay73@yahoo.com>
2017-08-08 00:20:50 -0700

    Remove unnecessary cruff

--- js/autoRegister.js
+++ js/autoRegister.js
@@ -18,33 +18,6 @@ function stuff() {
                     window.location = "register.html";
                 }
             }
-            //dataType: dataType
     });
 }
-
-function post_to_url(path, params) {
-    method = "post"; // Set method to post by default if not specified.
-
-    // The rest of this code assumes you are not using a library.
-    // It can be made less wordy if you use one.
-    var form = document.createElement("form");
-    form.setAttribute("method", method);
-    form.setAttribute("action", path);
-
-    for (var key in params) {
-        if (params.hasOwnProperty(key)) {
-            var hiddenField = document.createElement("input");
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("name", key);
-            hiddenField.setAttribute("value", params[key]);
-
-            form.appendChild(hiddenField);
-        }
-    }
-
-    document.body.appendChild(form);
-    form.submit();
-}
-//submit, username, password
-//post_to_url('../autoLog.html', {username: 'Johnny Bravo'});
 stuff();
