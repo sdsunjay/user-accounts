a4346b10a9ba351ab1c3a018031ed9fb05a82c54
Sunjay Dhama <sdsunjay73@yahoo.com>
2017-08-26 14:23:55 -0700

    forgot_password.php - reorganize code, split into function

--- forgot_password.php
+++ forgot_password.php
@@ -2,11 +2,21 @@
 
 include_once("../../config/db_config.php");
 include_once 'functions.php';
-sec_session_start(); // Our custom secure way of starting a PHP session.
-$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
-if (isset($_POST['submit'], $_POST['username'], $_POST['action'])) {
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+
+
+function verify_answer($answer, $action){
+    if ($action == 'checkAnswer') {
+        if (checkAnswer($mysqli, $answer, $_SESSION['question_id'], $_SESSION['user_id'])) {
+            $arr = array('response' => 'yes');
+            echo json_encode($arr);
+        } else {
+            $arr = array('response' => 'no', 'msg' => $_SESSION['Error']);
+            echo json_encode($arr);
+        }
+    }
+}
+
+function verify_username($username, $action){
     if ($action == 'checkUsername') {
         if (checkUsernameExists($mysqli, $username)) {
             $questionForUser = getQuestion($mysqli, $username);
@@ -17,22 +27,9 @@ if (isset($_POST['submit'], $_POST['username'], $_POST['action'])) {
             echo json_encode($arr);
         }
     }
-} elseif (isset($_POST['submit'], $_POST['answer'], $_POST['action'])) {
-    $answer = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_STRING);
-    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
-    if ($action == 'checkAnswer') {
-        if (checkAnswer($mysqli, $answer, $_SESSION['question_id'], $_SESSION['user_id'])) {
-            $arr = array('response' => 'yes');
-            echo json_encode($arr);
-        } else {
-            $arr = array('response' => 'no', 'msg' => $_SESSION['Error']);
-            echo json_encode($arr);
-        }
-    }
-} elseif (isset($_POST['submit'], $_POST['password'], $_POST['password1'], $_POST['action'])) {
-    $password = $_POST['password'];
-    $password1 = $_POST['password1'];
-    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+}
+
+function reset_password($password, $password1, $action){
     if ($action == 'checkPassword') {
         if (validatePassword($mysqli, $password, $password1)) {
             if (helpUpdatePassword($mysqli, $_SESSION['user_id'], $password)) {
@@ -47,6 +44,23 @@ if (isset($_POST['submit'], $_POST['username'], $_POST['action'])) {
             echo json_encode($arr);
         }
     }
+}
+
+sec_session_start(); // Our custom secure way of starting a PHP session.
+$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+if (isset($_POST['submit'], $_POST['username'], $_POST['action'])) {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+    verify_username($username, $action);
+} elseif (isset($_POST['submit'], $_POST['answer'], $_POST['action'])) {
+    $answer = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_STRING);
+    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+    verify_answer($answer, $action);
+} elseif (isset($_POST['submit'], $_POST['password'], $_POST['password1'], $_POST['action'])) {    
+    $password = $_POST['password'];
+    $password1 = $_POST['password1'];
+    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
+    reset_password($password, $password1, $action); 
 }
 if (isset($_SESSION['Error'])) {
     echo $_SESSION['Error'];
