74d7ee31279f11e4a22141108359046921d7ff3b
Sunjay Dhama <sdsunjay73@yahoo.com>
2017-08-28 11:34:11 -0700

    fixed syntax error

--- check.php
+++ check.php
@@ -4,29 +4,29 @@ include_once 'functions.php';
 
 sec_session_start(); // Our custom secure way of starting a PHP session.
 if(isset($_POST['submit'],$_POST['username'], $_POST['password'])) {
-   $response = 'no';
-   $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-   $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    $response = 'no';
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
-   if(is_null($username)) {
-      $msg = "Username is null";
-      $_SESSION['Error']= $message;
-   } elseif(is_null($password)) {
-      $msg = "Password is null";
-      $_SESSION['Error']= $msg;
-   } else { 
-      if(validatePassword($password, $password)){
-	  $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
-	  //using html purifier
-	  //$name = $purifier->purify($name);
-	  /* check connection */
-	  if (mysqli_connect_errno()) {
-	     $msg = "Connect: failed";
-	     $_SESSION['Error']= $msg;
-	  } elseif(login_check($mysqli) { //already logged in
-	     $response = 'yes';
-	     $msg = 'Success';
-	  } else{
+    if(is_null($username)) {
+	$msg = "Username is null";
+	$_SESSION['Error']= $message;
+    } elseif(is_null($password)) {
+	$msg = "Password is null";
+	$_SESSION['Error']= $msg;
+    } else {
+	if(validatePassword($password, $password)){
+	    $mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
+	    //using html purifier
+	    //$name = $purifier->purify($name);
+	    /* check connection */
+	    if (mysqli_connect_errno()) {
+		$msg = "Connect: failed";
+		$_SESSION['Error']= $msg;
+	    } elseif(login_check($mysqli)) { //already logged in
+		$response = 'yes';
+		$msg = 'Success';
+	    } else {
 		$user_id = getUserID($mysqli, $username);
 		if($user_id != 0){
 		    if(login($user_id, $password, $mysqli)) {
@@ -41,11 +41,11 @@ if(isset($_POST['submit'],$_POST['username'], $_POST['password'])) {
 		    }
 		} else {
 		    $msg = $username.' does not exist';
-		}   
+		}
 	    }
 	    $mysqli->close();
 	} else {
-	    $msg =  $_SESSION['Error'];	
+	    $msg =  $_SESSION['Error'];
 	}
     }
     $arr = array ('response'=> $response, 'msg' => $msg);
